bb54c1ed8fa16c043dedc59616974ece6debe601
DHARANITHARAN J <130481132+Dharanitharan-J@users.noreply.github.com>
2025-06-29 15:41:16 +0530

    tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
 
-// Get all tasks
 router.get('/', async (req, res) => {
   try {
     const tasks = await Task.find();
